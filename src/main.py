c70bfe3f8ceb5e4580c0b820dec1df306d2f53bb
SupRemaZie <damien@piter.at>
2025-10-28 09:49:34 +0100

    buff

--- src/main.py
+++ src/main.py
@@ -1,24 +1,28 @@
 import pygame
+from entitities.Buff import Buff
 
-# pygame setup
+# Initialisation de Pygame
 pygame.init()
-screen = pygame.display.set_mode((1920, 1080))
+screen = pygame.display.set_mode((1200, 720))
 clock = pygame.time.Clock()
 running = True
+
+# Créer le buff UNE SEULE FOIS (pas dans la boucle)
+buff = Buff((100, 100))
+
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
+    # Effacer l'écran
     screen.fill("black")
 
-    # RENDER YOUR GAME HERE
+    # Dessiner le buff
+    screen.blit(buff.image, buff.rect)  # Utilise buff.image et buff.rect
 
-    # flip() the display to put your work on screen
+    # Mettre à jour l'affichage
     pygame.display.flip()
-
+    clock.tick(60)  # Limiter à 60 FPS
 
 pygame.quit()
